3ec77c0f9b2cb514939735e0800f8ff3355e8f21
OliverYecute <yecuteoliver@gmail.com>
2026-05-07 17:44:56 -0600

    Implementa carga dinámica de casas, autocompletado de propietarios y diseño inicial de configuración de cuota

--- src/proyecto_condominio/ui/RegistroPagoViewController.java
+++ src/proyecto_condominio/ui/RegistroPagoViewController.java
@@ -12,6 +12,11 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.time.LocalDate;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import proyecto_condominio.src.Config;
 /**
  * FXML Controller class
  *
@@ -51,12 +56,144 @@ public class RegistroPagoViewController implements Initializable {
     @FXML
     private Label lblEstado;
 
+    @FXML
+    private Button btnCerrar;
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public void initialize(URL url, ResourceBundle rb) {
+
+    cbMes.getItems().addAll(
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    );
+
+    cbAnio.getItems().addAll(
+        "2025",
+        "2026",
+        "2027",
+        "2028"
+    );
+
+    LocalDate fechaActual = LocalDate.now();
+
+    int mesActual = fechaActual.getMonthValue();
+    cbMes.getSelectionModel().select(mesActual - 1);
+
+    int anioActual = fechaActual.getYear();
+    cbAnio.setValue(String.valueOf(anioActual));
     
+    cargarCasas();
+}
+private void cargarCasas() {
+
+    String sql = """
+    SELECT numero_casa
+    FROM propietario
+    ORDER BY numero_casa ASC
+    """;
+
+    try (
+        Connection conn = Config.getConexion();
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+    ) {
+
+        while (rs.next()) {
+
+            cbCasa.getItems().add(
+                rs.getString("numero_casa")
+            );
+
+        }
+
+    } catch (Exception e) {
+
+        lblEstado.setText("Error al cargar casas");
+
+        System.out.println(e.getMessage());
+    }
+}
+    @FXML
+private void seleccionarCasa() {
+
+    String sql = """
+        SELECT 
+            primer_nombre,
+            segundo_nombre,
+            tercer_nombre,
+            primer_apellido,
+            segundo_apellido,
+            telefono
+        FROM propietario
+        WHERE numero_casa = ?
+    """;
+
+    try (
+        Connection conn = Config.getConexion();
+        PreparedStatement ps = conn.prepareStatement(sql);
+    ) {
+
+        ps.setInt(
+            1,
+            Integer.parseInt(cbCasa.getValue())
+        );
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+
+            String nombre = rs.getString("primer_nombre");
+
+String segundoNombre = rs.getString("segundo_nombre");
+String tercerNombre = rs.getString("tercer_nombre");
+
+if (segundoNombre != null) {
+    nombre += " " + segundoNombre;
+}
+
+if (tercerNombre != null) {
+    nombre += " " + tercerNombre;
+}
+
+txtNombre.setText(nombre);
+
+            String apellido = rs.getString("primer_apellido");
+
+String segundoApellido = rs.getString("segundo_apellido");
+
+if (segundoApellido != null) {
+    apellido += " " + segundoApellido;
+}
+
+txtApellido.setText(apellido);
+
+            txtTelefono.setText(
+                rs.getString("telefono")
+            );
+
+            txtCuota.setText("Q1500");
+
+        }
+
+    } catch (Exception e) {
+
+        lblEstado.setText(
+            "Error al cargar propietario"
+        );
+
+        System.out.println(e.getMessage());
+    }
+}
 }
