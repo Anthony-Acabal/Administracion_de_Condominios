b5367b531f7d8d0df70ff0be709ff1452ef07fde
OliverYecute <yecuteoliver@gmail.com>
2026-05-12 14:41:28 -0600

    Implementacion de clases, ventanas emergentes y validaciones en Pantalla 4

---
+++ src/proyecto_condominio/model/Cuota.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_condominio.model;
+
+public class Cuota {
+
+    private int idCuota;
+
+    private double montoCuota;
+
+    public Cuota() {
+
+    }
+
+    public int getIdCuota() {
+        return idCuota;
+    }
+
+    public void setIdCuota(int idCuota) {
+        this.idCuota = idCuota;
+    }
+
+    public double getMontoCuota() {
+        return montoCuota;
+    }
+
+    public void setMontoCuota(double montoCuota) {
+        this.montoCuota = montoCuota;
+    }
+}
---
+++ src/proyecto_condominio/model/Propietario.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_condominio.model;
+
+public class Propietario {
+
+    private int idPropietario;
+
+    private int numeroCasa;
+
+    private String primerNombre;
+
+    private String segundoNombre;
+
+    private String tercerNombre;
+
+    private String primerApellido;
+
+    private String segundoApellido;
+
+    private String telefono;
+
+    public Propietario() {
+
+    }
+
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getTercerNombre() {
+        return tercerNombre;
+    }
+
+    public void setTercerNombre(String tercerNombre) {
+        this.tercerNombre = tercerNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
