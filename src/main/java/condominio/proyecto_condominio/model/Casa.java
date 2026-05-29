2e407f4df2516373ad3fb2492b24448a0c94f512
OliverYecute <yecuteoliver@gmail.com>
2026-05-29 12:23:10 -0600

    Restaurar archivos eliminados accidentalmente

---
+++ src/main/java/condominio/proyecto_condominio/model/Casa.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package condominio.proyecto_condominio.model;
+
+public class Casa {
+
+    public Casa(
+            int idCasa,
+            int numeroCasa,
+            String estado
+    ) {
+
+        this.idCasa = idCasa;
+        this.numeroCasa = numeroCasa;
+        this.estado = estado;
+    }
+
+    private int idCasa;
+
+    private int numeroCasa;
+
+    private String estado;
+
+    public Casa() {
+
+    }
+
+    public int getIdCasa() {
+        return idCasa;
+    }
+
+    public void setIdCasa(int idCasa) {
+        this.idCasa = idCasa;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
